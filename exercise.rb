7950a9ad94de4597a809bb99ed524da5a452d39f
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-10 15:40:56 -0400

    Add second solution using each_with_index

--- exercise.rb
+++ exercise.rb
@@ -3,17 +3,25 @@ en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
 
 new_hash = {}
-array_counter = 0
 
-#digits becomes the keys
-#en and fr become the values
+# Solution 1
 
-digits.each do |digit|
-  new_hash[digit.to_i] = {
-    french: fr[array_counter],
-    english: en[array_counter]
+# array_counter = 0
+
+# digits.each do |digit|
+#   new_hash[digit.to_i] = {
+#     french: fr[array_counter],
+#     english: en[array_counter]
+#   }
+#   array_counter += 1
+# end
+
+# Solution 2
+digits.each_with_index do |digit, index|
+  new_hash[digit.to_i] =  {
+    french: fr[index],
+    english: en[index]
   }
-  array_counter += 1
 end
 
 puts new_hash
